27cff49fb4fee2c2816332ff02d246299d3219d3
malcomdee <89889959+malcomdee@users.noreply.github.com>
2021-11-18 03:26:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)   
-
-
-
-
-
-
-
+app = Flask(__name__)
 
 @app.route('/')          
 
 def hello_world():
 
-    return  render_template ("registro.html") 
-    
+    return  render_template ("inicio2.html") 
+
+@app.route('/registro')          
+
+def registro():
+
+    return  render_template ("registro.html")    
+
+
+@app.route('/iniciado')  
+def iniciado():
+
+    return render_template("iniciado.html")
+
+
 @app.route('/nombre', methods=["post"])
 def nombre():
     name=request.form["name"]
     return render_template("nombre2.html",name=name)
 
-@app.route('/inicio')          
-
-def inicio():
-
-    return  render_template ("inicio2.html") 
 
 @app.route('/quienes')          
 
@@ -32,43 +33,6 @@ def quienes():
 
     return  render_template ("quienessomos.html")
 
-@app.route('/calculadora')          
-
-def calculator():
-
-    return  render_template ("index2.html")
-
-@app.route('/calculo', methods=["POST"])       
-
-def resultado():
-
-    nr1 = request.form["nr1"]
-
-    a = request.form["a"]
-
-    nr2 = request.form["nr2"]
-    if a == "+":
-
-        result= int(nr1)+int(nr2)
-
-    if a == "-":
-
-        result= int(nr1)-int(nr2)
-
-    if a == "*":
-
-        result= int(nr1)*int(nr2)
-
-    if a == "/":
-
-        result= int(nr1)/int(nr2)
-
-    if a == "**":
-
-        result= int(nr1)**int(nr2)
-
-
-    return  render_template ("result2.html", nr1 = nr1 , a = a, nr2 = nr2, result=result) 
 
 
 
@@ -78,5 +42,4 @@ def resultado():
 
 if __name__=="__main__":   
 
-    app.run(debug=True)    
-
+    app.run(debug=True)   
